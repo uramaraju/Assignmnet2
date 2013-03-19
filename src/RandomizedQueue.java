c30870560160c2729a8d922c3fd6407aa4b1a2f9
UmaSankar Ramaraju <ramaraju.umasankar@gmail.com>
2013-03-18 23:40:43 -0500

    rand queue

---
+++ src/RandomizedQueue.java
@@ -0,0 +1,39 @@
+import java.util.Iterator;
+
+public class RandomizedQueue<Item> implements Iterable<Item>
+{
+    public RandomizedQueue()
+    {
+
+    }
+
+    public boolean isEmpty()
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    public int size()
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    public void enqueue(Item item)
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    public Item dequeue()
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    public Item sample()
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    public Iterator<Item> iterator()
+    {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ src/Subset.java
@@ -0,0 +1,9 @@
+
+public class Subset
+{
+    public static void main(String[] args)
+    {
+        while (!StdIn.isEmpty())
+        StdOut.println(StdIn.readString());
+    }
+}
