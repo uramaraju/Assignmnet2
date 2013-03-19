d1df1047a7dc4e6af0e6bb05bdc0efd83707bc50
UmaSankar Ramaraju <ramaraju.umasankar@gmail.com>
2013-03-18 23:04:20 -0500

    tests

--- src/Deque.java
+++ src/Deque.java
@@ -1,8 +1,118 @@
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 
 public class Deque<Item> implements Iterable<Item>
 {
+
+    private int size;
+    private Node<Item> first;
+    private Node<Item> last;
+
+    public Deque()
+    {
+        first = last = null;
+        size = 0;
+    }
+
+    public void removeLast()
+    {
+        assertIsNotEmpty();
+        if (size > 1)
+        {
+            last.getPrevious().setNext(null);
+            last = last.getPrevious();
+        } else
+        {
+            last = first = null;
+
+        }
+        size--;
+    }
+
+    private void assertIsNotEmpty()
+    {
+        if (isEmpty())
+        {
+            throw new NoSuchElementException();
+        }
+    }
+
+    @Override
+    public Iterator<Item> iterator()
+    {
+        return new NodeIterator<Item>(first);
+
+    }
+
+    public boolean isEmpty()
+    {
+        return size == 0;
+    }
+
+    public int size()
+    {
+        return size;
+    }
+
+    public void addFirst(Item i)
+    {
+        assertItemIsNotNull(i);
+        Node<Item> node = new Node<Item>();
+        node.setItem(i);
+
+        if (size == 0)
+        {
+            first = last = node;
+        } else
+        {
+            first.previous = node;
+            node.next = first;
+            first = node;
+        }
+        size++;
+    }
+
+    private void assertItemIsNotNull(Item i)
+    {
+        if (null == i)
+        {
+            throw new NullPointerException("item is null.");
+        }
+    }
+
+    public void addLast(Item i)
+    {
+        assertItemIsNotNull(i);
+        Node<Item> node = new Node<Item>();
+        node.setItem(i);
+
+        if (size == 0)
+        {
+            first = last = node;
+        } else
+        {
+            last.next = node;
+            node.previous = last;
+            last = node;
+        }
+        size++;
+    }
+
+    public void removeFirst()
+    {
+        assertIsNotEmpty();
+        if (size > 1)
+        {
+            first.getNext().setPrevious(null);
+            first = first.getNext();
+        } else
+        {
+            last = first = null;
+
+        }
+        size--;
+    }
 
     private static class Node<Item>
     {
@@ -45,68 +155,33 @@ public class Deque<Item> implements Iterable<Item>
         }
     }
 
-    @Override
-    public Iterator<Item> iterator()
+    private static class NodeIterator<Item> implements Iterator<Item>
     {
-        return null;
-    }
+        private Node<Item> current;
 
-    private int size;
-    private Node<Item> first;
-    private Node<Item> last;
-
-    public Deque()
-    {
-        first = last = null;
-        size = 0;
-    }
-
-    public boolean isEmpty()
-    {
-        return size == 0;
-    }
-
-    public int size()
-    {
-        return size;
-    }
-
-    public void addFirst(Item i)
-    {
-        Node<Item> node = new Node<Item>();
-        node.setItem(i);
-
-        if (size == 0)
+        private NodeIterator(Node<Item> node)
         {
-            first = last = node;
+            this.current = node;
         }
-        else
-        {
-            first.previous = node;
-            node.next = first;
-            first = node;
-        }
-    }
 
-    public void addLast(Item i)
-    {
-        Node<Item> node = new Node<Item>();
-        node.setItem(i);
-
-        if (size == 0)
+        @Override
+        public boolean hasNext()
         {
-            first = last = node;
+            return current != null;
         }
-        else
-        {
-            last.next = node;
-            node.previous = last;
-            last = node;
-        }
-    }
 
-    public void removeFirst()
-    {
-        throw new UnsupportedOperationException();
+        @Override
+        public Item next()
+        {
+            Item i = current.getItem();
+            current = current.next;
+            return i;
+        }
+
+        @Override
+        public void remove()
+        {
+            throw new UnsupportedOperationException();
+        }
     }
 }
--- testsrc/DequeTest.java
+++ testsrc/DequeTest.java
@@ -1,12 +1,116 @@
-/**
- * Created with IntelliJ IDEA.
- * User: umasankarramaraju
- * Date: 3/18/13
- * Time: 9:43 PM
- * To change this template use File | Settings | File Templates.
- */
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
-@TestClass()
+import java.util.NoSuchElementException;
+
+import static org.junit.Assert.*;
 public class DequeTest
 {
+    @Test
+    public void defaultSizeIsZeroAndEmpty()
+    {
+        Deque<Integer> deque = new Deque<Integer>();
+        assertEquals(0, deque.size());
+        assertTrue(deque.isEmpty());
+    }
+
+    @Test
+    public void addFirstWhenEmpty()
+    {
+        Deque<Integer> deque = new Deque<Integer>();
+        deque.addFirst(2);
+        assertEquals(1, deque.size());
+        assertFalse(deque.isEmpty());
+    }
+
+    @Test
+    public void addLastWhenEmpty()
+    {
+        Deque<Integer> deque = new Deque<Integer>();
+        deque.addLast(2);
+        assertEquals(1, deque.size());
+        assertFalse(deque.isEmpty());
+    }
+
+    @Test
+    public void iteratorTest()
+    {
+        Deque<Integer> deque = new Deque<Integer>();
+        for (int i = 0; i < 10; i++)
+        {
+            deque.addFirst(i+1);
+        }
+        int expected = 10;
+        for(int i: deque)
+        {
+            assertEquals(expected, i);
+            expected--;
+        }
+        assertEquals(expected,0);
+    }
+
+    public  @Rule ExpectedException exception = ExpectedException.none();
+    @Test
+    public void cantAddNullItem()
+    {
+        Deque<String> deque = new Deque<String>();
+        exception.expect(NullPointerException.class);
+        exception.expectMessage("item is null.");
+        deque.addFirst(null);
+    }
+
+    @Test
+    public void remove()
+    {
+        Deque<Integer> deque = new Deque<Integer>();
+        for (int i = 0; i < 10; i++)
+        {
+            deque.addFirst(i+1);
+        }
+        //added 10 .. 1, removed 10, 1
+        deque.removeFirst();
+        deque.removeLast();
+        int expected = 9;
+        for(int i: deque)
+        {
+            assertEquals(expected, i);
+            expected--;
+        }
+        assertEquals(8,deque.size());
+        assertEquals(expected,1);
+    }
+
+    @Test
+    public void removeFromEmptyQueue()
+    {
+        Deque<String> deque = new Deque<String>();
+        exception.expect(NoSuchElementException.class);
+        deque.removeFirst();
+
+    }
+    @Test
+    public void removeLastFromEmptyQueue()
+    {
+        Deque<String> deque = new Deque<String>();
+        exception.expect(NoSuchElementException.class);
+        deque.removeLast();
+
+    }
+
+    @Test
+    public void removeFromIteratorThrowsException()
+    {
+        Deque<Integer> deque = new Deque<Integer>();
+        for (int i = 0; i < 10; i++)
+        {
+            deque.addFirst(i+1);
+        }
+        exception.expect(UnsupportedOperationException.class);
+        while (deque.iterator().hasNext())
+        {
+            deque.iterator().remove();
+
+        }
+    }
 }
